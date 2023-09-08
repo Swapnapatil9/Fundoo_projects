082083887aa929a86bd8dce7b351311b68ad54f2
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-09-08 17:43:27 +0530

    Added: Delete, permanent deleting and Page reload using useNavigate is done

--- src/Components/takeNoteThree/gridList/GridList.jsx
+++ src/Components/takeNoteThree/gridList/GridList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@mui/material'
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
@@ -9,9 +9,15 @@ import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import "./GridList.css"
 import ColorPopper from '../../Popper/ColorPopper';
 
-function GridList({ color, notes, deleting, updateArchive, getData, permanentDeleting}) {
+function GridList({ color, notes, deleting, updateArchive, getData, permanentDeleting }) {
+
+  const [isToggle, setIsToggle] = useState(false);
+  const dataDeleting = () => {
+    setIsToggle(!isToggle)
+  }
+
   return (
-    <Grid container className='main-container4' style={{ width: "600px", backgroundColor: notes.color}}>
+    <Grid container className='main-container4' style={{ width: "600px", backgroundColor: notes.color }}>
 
       <Grid container className='contents' spacing={2}>
         <Grid item xs={11} className='headline' style={{ textAlign: 'left' }}>
@@ -21,18 +27,26 @@ function GridList({ color, notes, deleting, updateArchive, getData, permanentDel
           <PushPinOutlinedIcon id="icon">pin note</PushPinOutlinedIcon>
         </Grid>
 
-        <Grid item xs={12} className='take-note' style={{ textAlign: 'left' ,marginLeft:'5px'}}>
+        <Grid item xs={12} className='take-note' style={{ textAlign: 'left', marginLeft: '5px' }}>
           <div>{notes.description}</div>
         </Grid>
 
-        <Grid item xs={5} className='miu-icons' style={{ textAlign: 'left', paddingRight: '5px' }}>
-          <DoneOutlinedIcon />
-          <PersonAddAltOutlinedIcon />
-          <ColorPopper action='edit' NoteId={notes.id} color={color} getData={getData}/>
-          <InsertPhotoOutlinedIcon />
-          <ArchiveOutlinedIcon NoteId={notes.id} onClick={()=>updateArchive(notes.id)}/>
-          <DeleteOutlinedIcon NoteId={notes.id} onClick={()=>{deleting(notes.id); permanentDeleting(notes.id)}}/>
-        </Grid>
+        {isToggle ?
+          <Grid item xs={5} className='miu-icons' style={{ textAlign: 'left', paddingRight: '5px' }}>
+            <PersonAddAltOutlinedIcon />
+            <ColorPopper action='edit' NoteId={notes.id} color={color} getData={getData} />
+            <InsertPhotoOutlinedIcon />
+            <ArchiveOutlinedIcon NoteId={notes.id} onClick={() => updateArchive(notes.id)} />
+            <DeleteOutlinedIcon NoteId={notes.id} onClick={() => { deleting(notes.id) }} />
+          </Grid> :
+          <Grid item xs={5} className='miu-icons' style={{ textAlign: 'left', paddingRight: '5px' }}>
+            <PersonAddAltOutlinedIcon />
+            <ColorPopper action='edit' NoteId={notes.id} color={color} getData={getData} />
+            <InsertPhotoOutlinedIcon />
+            <ArchiveOutlinedIcon NoteId={notes.id} onClick={() => updateArchive(notes.id)} />
+            <DeleteOutlinedIcon NoteId={notes.id} onClick={() => { permanentDeleting(notes.id) }} />
+          </Grid>
+        }
       </Grid>
     </Grid>
   )
